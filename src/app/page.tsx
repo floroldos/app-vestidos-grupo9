8066983952405200f745bb5c423357f42052dbca
amandaseara <amandadenissese@gmail.com>
2025-11-20 18:26:26 -0300

    arreglo calendario

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function Home() {
   const [startDate, setStartDate] = useState<Date | null>(null);
@@ -183,5 +184,3 @@ export default function Home() {
     </div>
   );
 }
-
-
