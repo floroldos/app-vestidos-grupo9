c3c8f9e64973f9136dfeaeb0b9a00b7acab89beb
luvalciukas <lvalciukas2003@gmail.com>
2025-11-30 23:03:30 -0300

    fix use envvariables for admin credentials

--- tests/testData/users.ts
+++ tests/testData/users.ts
@@ -1,6 +1,6 @@
 export const users = {
     admin: {
-        user: process.env.ADMIN_USER ?? 'admin',
-        pass: process.env.ADMIN_PASS ?? 'admin123'
+        user: process.env.ADMIN_USER!,
+        pass: process.env.ADMIN_PASS!
     }
 };
